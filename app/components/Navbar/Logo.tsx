1411b5e17a3e062438d11e9fc87bb40260f19c75
basilshaikh <basilshaikh16@gmail.com>
2024-09-11 22:40:04 +0500

    Navbar categories UI

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -6,7 +6,10 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <div className="px-4 py-2 bg-taupe rounded-full">
+    <div
+      className="px-4 py-2 bg-taupe rounded-full cursor-pointer"
+      onClick={() => router.push("/")}
+    >
       <h2 className="text-soft-cream text-3xl">
         Wander<span className="text-sandy-brown font-bold">Nest</span>
       </h2>
